9a7c401aa66dc7e88c3c821452d689fd7a105fa2
Alex Conley <>
2024-12-09 21:02:00 -0700

    Day 9 part 2.

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,13 +1,25 @@
 // Day 9: Disk Fragmenter
 fn main() -> std::io::Result<()> {
     let input = rust_advent::read_file_as_string("09")?;
-    let mut disk = Disk::new_from_string(&input);
 
-    // Part 1: defrag and checksum.
-    disk.defrag();
-    println!("Checksum after defrag: {}", disk.checksum());
-
+    println!("Checksum after defrag: {}", defrag_and_checksum(&input));
+    println!(
+        "Checksum after defrag files: {}",
+        defrag_and_checksum_files(&input)
+    );
     Ok(())
+}
+
+fn defrag_and_checksum(input: &str) -> u64 {
+    let mut disk = Disk::new_from_string(input);
+    disk.defrag();
+    disk.checksum()
+}
+
+fn defrag_and_checksum_files(input: &str) -> u64 {
+    let mut disk = DiskList::new_from_string(input);
+    disk.defrag();
+    disk.checksum()
 }
 
 struct Disk {
@@ -17,10 +29,7 @@ struct Disk {
 
 impl Disk {
     fn new_from_string(input: &str) -> Self {
-        let elems: Vec<u32> = input
-            .chars()
-            .filter_map(|c| c.to_digit(10))
-            .collect();
+        let elems: Vec<u32> = input.chars().filter_map(|c| c.to_digit(10)).collect();
         let n_blocks = elems.iter().sum::<u32>() as usize;
         let mut blocks = vec![u16::MAX; n_blocks];
 
@@ -74,6 +83,109 @@ impl Disk {
             .map(|(i, b)| i * (b as usize))
             .sum::<usize>() as u64
     }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct DiskList {
+    files: Vec<File>,
+    free_space: Vec<FreeSpace>,
+}
+
+impl DiskList {
+    fn new_from_string(input: &str) -> Self {
+        let elems: Vec<u32> = input.chars().filter_map(|c| c.to_digit(10)).collect();
+
+        let mut files = Vec::with_capacity(elems.len() / 2 + 1);
+        let mut free_space = Vec::with_capacity(elems.len() / 2);
+        enum ParseState {
+            Empty,
+            Data,
+        }
+        let mut state = ParseState::Data;
+        let mut id = 0;
+        let mut idx = 0;
+        for elem in elems {
+            match state {
+                ParseState::Empty => {
+                    free_space.push(FreeSpace {
+                        start: idx,
+                        size: elem as usize,
+                    });
+                    state = ParseState::Data;
+                }
+                ParseState::Data => {
+                    files.push(File {
+                        id,
+                        start: idx,
+                        size: elem as usize,
+                    });
+                    id += 1;
+                    state = ParseState::Empty;
+                }
+            }
+            idx += elem as usize;
+        }
+
+        Self { files, free_space }
+    }
+
+    fn defrag(&mut self) {
+        self.files.iter_mut().rev().for_each(|fl| {
+            if let Some(free) = self
+                .free_space
+                .iter_mut()
+                .find(|free| free.start < fl.start && free.size >= fl.size)
+            {
+                // We found a free chunk to move the file into.
+                // Do so, reducing the free space chunk.
+                fl.start = free.start;
+                free.start += fl.size;
+                free.size -= fl.size;
+
+                // This implementation is not quite right -- we need to create a new free space chunk
+                // in the space we moved out of, and possibly merge with adjacent free space chunks.
+                // However, that doesn't affect the answer:
+                //  1) The checksum doesn't care about the free space.
+                //  2) Because we are iterating backwards over the files, no other file can move
+                //    into the space we just moved out of.
+            }
+        });
+
+        self.files.sort_by_key(|fl| fl.start);
+        self.free_space.retain(|free| free.size > 0);
+    }
+
+    fn checksum(&self) -> u64 {
+        self.files.iter().map(|fl| fl.checksum()).sum()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct File {
+    id: u16,
+    start: usize,
+    // A size of 0 means the file is deactivated.
+    size: usize,
+}
+
+impl File {
+    fn checksum(&self) -> u64 {
+        if self.size == 0 {
+            0
+        } else {
+            // There is surely a closed form for this.
+            (self.start..(self.start + self.size))
+                .into_iter()
+                .map(|idx| idx as u64 * self.id as u64)
+                .sum()
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct FreeSpace {
+    start: usize,
+    size: usize,
 }
 
 #[cfg(test)]
@@ -149,5 +261,12 @@ mod tests {
         let mut disk = Disk::new_from_string("2333133121414131402");
         disk.defrag();
         assert_eq!(disk.checksum(), 1928);
+    }
+
+    #[test]
+    fn test_defrag_files() {
+        let mut disk = DiskList::new_from_string("2333133121414131402");
+        disk.defrag();
+        assert_eq!(disk.checksum(), 2858);
     }
 }
